e36c24c8b49a91650ea2ca649872f2b40ae48a65
Evgeniy Koval <glggboko@gmail.com>
2021-07-01 17:09:13 +0300

    Add recursive algorithm. Working

--- test2/constants.js
+++ test2/constants.js
@@ -5,35 +5,48 @@ const WALL = '#';
 const maze = [
 
     ['#','#','#','#','#','#','#','#','#'],
-  
     ['#','+','+','+','#','+','+','+','#'],
-  
     ['#','+','#','+','#','+','#','+','#'],
-  
     ['+','+','#','+','0','+','#','+','#'],
-  
     ['#','#','#','+','#','#','#','#','#'],
-  
     ['#','#','+','+','#','#','#','#','#'],
-  
     ['#','#','+','#','#','#','#','#','#'],
-  
     ['#','#','#','#','#','#','#','#','#'],
   
 ];
 
-const directions = {
-    top: {x: 0, y: -1},
-    left: {x: -1, y: 0},
-    right: {x: 1, y: 0},
-    bottom: {x: 0, y: 1},
-}
+const DIRECTIONS = [
+    { 
+        name: 'top', 
+        x: 0,
+        y: -1, 
+        exclude: 'bottom'
+    },
+    { 
+        name: 'left', 
+        x: -1,
+        y: 0, 
+        exclude: 'right'
+    },
+    { 
+        name: 'right', 
+        x: 1,
+        y: 0, 
+        exclude: 'left'
+    },
+    { 
+        name: 'bottom', 
+        x: 0,
+        y: 1, 
+        exclude: 'top'
+    },
+]
 
 
 module.exports = {
     HERO,
     WAY,
     WALL,
+    DIRECTIONS,
     maze,
-    directions
 };
